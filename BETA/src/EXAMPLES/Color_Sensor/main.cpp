c20b60117af9aedb95981f96b0a44dd005b168b8
João Ferreira <100808096+JoaoFerreira123@users.noreply.github.com>
2024-08-15 23:59:04 -0300

    color works

---
+++ BETA/src/EXAMPLES/Color_Sensor/main.cpp
@@ -0,0 +1,37 @@
+#include <Wire.h>
+#include <Adafruit_TCS34725.h>
+
+/* Example code for the Adafruit TCS34725 breakout library */
+
+/* Connect SCL    to analog 5
+   Connect SDA    to analog 4
+   Connect VDD    to 3.3V DC
+   Connect GROUND to common ground */
+
+/* Initialise with default values (int time = 2.4ms, gain = 1x) */
+// Adafruit_TCS34725 tcs = Adafruit_TCS34725();
+
+/* Initialise with specific int time and gain values */
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_1X);
+
+void setup(void) {
+  Serial.begin(115200);
+  
+  if (tcs.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1);
+  }
+  // Now we're ready to get readings!
+}
+
+void loop(void) {
+  float red, green, blue;
+  tcs.getRGB(&red, &green, &blue);
+
+  Serial.print("R: "); Serial.print(red, DEC); Serial.print(" ");
+  Serial.print("G: "); Serial.print(green, DEC); Serial.print(" ");
+  Serial.print("B: "); Serial.print(blue, DEC); Serial.print(" ");
+  Serial.println(" ");
+}
