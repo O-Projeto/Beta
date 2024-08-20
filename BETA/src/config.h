56c467a47a3ab442a52963ffc057266d1c67b0ab
JoaoFerreira123 <joaoferreira.2020@alunos.utfpr.edu.br>
2024-08-20 14:40:38 -0300

    config

---
+++ BETA/src/config.h
@@ -0,0 +1,48 @@
+#pragma once
+
+
+namespace Beta
+{
+    const int motor1 = 4;
+    const int motor2 = 16;
+    const int motor3 = 17;
+    const int motor4 = 18;
+    const int motor5 = 19;
+    const int motor6 = 21;
+    const int LED_PCB = 27;
+    const int LED_Beta = 32;
+
+}
+
+
+
+namespace Cenario{
+    const int botaoAzul = 35;
+    const int botaoVerde = 34;
+    const int botaoVermelho = 39;
+    const int botaoAmarelo = 36;
+    const int LED_1 = 25;
+    const int LED_2 = 26;
+    const int PWMA = 33;
+    const int AIN1 = 27;
+    const int AIN2 = 14;
+    const int sensorInicio = 5;
+    const int sensorFim = 17;
+    const int sensorRed = 16;
+    const int sensorBlue = 12;
+    const int sensorGreen = 13;
+    const int LedSensorCor = 3;
+
+}
+
+namespace Joystick{
+    const int pot1 = 0;
+    const int pot2 = 1;
+    const int pot3 = 2;
+    const int pot4 = 3;
+    const int pot5 = 4;
+    const int botaoGarra = 10;
+ 
+}
+
+
